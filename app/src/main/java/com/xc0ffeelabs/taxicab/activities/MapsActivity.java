58c6a1cb96e50d8a34d4c29429d5484a947067ab
Nayan Kumar K <qtc746@motorola.com>
2016-03-11 23:54:24 -0800

    Move maps related code to a Fragment

--- app/src/main/java/com/xc0ffeelabs/taxicab/activities/MapsActivity.java
+++ app/src/main/java/com/xc0ffeelabs/taxicab/activities/MapsActivity.java
@@ -1,49 +1,30 @@
 package com.xc0ffeelabs.taxicab.activities;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 import com.xc0ffeelabs.taxicab.R;
+import com.xc0ffeelabs.taxicab.fragments.MapsFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MapsActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener{
-
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 1;
+public class MapsActivity extends AppCompatActivity {
 
     @Bind(R.id.nvView) NavigationView mNavView;
     @Bind(R.id.toolbar) Toolbar mToolbar;
     @Bind(R.id.dl_nav_drawer) DrawerLayout mDrawer;
 
     private ActionBarDrawerToggle mDrawerToggle;
-    private GoogleMap mMap;
-    private SupportMapFragment mMapFragment;
-    private GoogleApiClient mApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,13 +33,14 @@ public class MapsActivity extends AppCompatActivity implements
 
         ButterKnife.bind(this);
 
-        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-
         setSupportActionBar(mToolbar);
 
         setupNavDrawer();
 
-        setupMap();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        MapsFragment mapsFragment = MapsFragment.newInstance();
+        ft.replace(R.id.fm_placeholder, mapsFragment);
+        ft.commit();
     }
 
     private void setupNavDrawer() {
@@ -123,142 +105,5 @@ public class MapsActivity extends AppCompatActivity implements
         Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
         finish();
-    }
-
-    private void setupMap() {
-        if (mMapFragment != null) {
-            mMapFragment.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    loadMap(googleMap);
-                }
-            });
-        } else {
-            Toast.makeText(this, "Error: map fragment is null", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void loadMap(GoogleMap googleMap) {
-        mMap = googleMap;
-        if (mMap == null) {
-            Toast.makeText(this, "Map failed to load!", Toast.LENGTH_SHORT).show();
-            return;
-        } else {
-            getMyLocation();
-        }
-    }
-
-    private void getMyLocation() {
-        try {
-            mMap.setMyLocationEnabled(true);
-            setupApiClient();
-        } catch (SecurityException e) {
-            throw e;
-        }
-    }
-
-    private void setupApiClient() {
-        if (mApiClient == null) {
-            mApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(LocationServices.API)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
-            connectClient();
-        }
-    }
-
-    private void connectClient() {
-        if (isGooglePlayServicesAvailable() && mApiClient != null) {
-            mApiClient.connect();
-        }
-    }
-
-    private void disconnectClient() {
-        if (mApiClient != null) {
-            mApiClient.disconnect();
-        }
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        try {
-            Location location = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
-            if (location != null) {
-                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
-                mMap.animateCamera(cameraUpdate);
-                startLocationUpdates();
-            } else {
-                Toast.makeText(this, "Cound't retrieve current location. Please enable GPS location",
-                        Toast.LENGTH_SHORT).show();
-            }
-        } catch (SecurityException e) {
-            throw e;
-        }
-    }
-
-    private void startLocationUpdates() {
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        if (i == CAUSE_SERVICE_DISCONNECTED) {
-            Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
-        } else if (i == CAUSE_NETWORK_LOST) {
-            Toast.makeText(this, "Network lost. Please re-connect.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(this,
-                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            Toast.makeText(getApplicationContext(),
-                    "Sorry. Location services not available to you", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        connectClient();
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        disconnectClient();
-        super.onStop();
-    }
-
-    private boolean isGooglePlayServicesAvailable() {
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-        int result = apiAvailability.isGooglePlayServicesAvailable(this);
-        if (result != ConnectionResult.SUCCESS) {
-            Dialog errorDialog = apiAvailability.getErrorDialog(this, result,
-                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
-            errorDialog.show();
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case CONNECTION_FAILURE_RESOLUTION_REQUEST:
-                if (resultCode == RESULT_OK) {
-                    connectClient();
-                    break;
-                }
-        }
     }
 }
